3e7ae45f74bb408e1cdc4e20d274756137418ac9
Unix&Me <unixnme@gmail.com>
2022-02-03 14:42:42 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import argparse
+
+if __name__ == '__main__':
+    default_config = {
+        'batch_size': 1024,
+        'sep': '|',
+    }
+
+    parser = argparse.ArgumentParser('brute force nearest neighbor search')
+    parser.add_argument('--query_file', required=True)
+    parser.add_argument('--batch_size', type=int, default=default_config['batch_size'],
+                        help=f'query batch size; default: {default_config["batch_size"]}')
+    parser.add_argument('--database_file', required=True)
+    parser.add_argument('--sep', default=default_config['sep'],
+                        help=f'separator for vector representation; default: {default_config["sep"]}')
+    parser.add_argument('--dim', type=int, required=True, help='expected vector dimension; all non-compliant entry will be ignored')
+    parser.add_argument('--backend', choices=['pytorch', 'faiss'], default='pytorch',
+                        help='brute force search backend')
+
+    args = parser.parse_args()

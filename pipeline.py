1c6d3135914aa535c70c6298a2c52aa146c18300
Unix&Me <unixnme@gmail.com>
2022-02-21 04:54:47 -0800

    add pipeline

---
+++ pipeline.py
@@ -0,0 +1,110 @@
+import sys
+import multiprocessing as mp
+import argparse
+
+
+class Pipeline(mp.Process):
+    def __init__(self, queue_in:mp.Queue, queue_out:mp.Queue, work):
+        super().__init__()
+        self.queue_in = queue_in
+        self.queue_out = queue_out
+        self.work = work
+
+    def run(self):
+        for item in iter(self.queue_in.get, None):
+            print(f"{self}: {self.queue_out.qsize()}", file=sys.stderr)
+            self.queue_out.put(self.work(item))
+        self.queue_out.put(None)
+
+
+class Adapter(mp.Process):
+    def __init__(self, queue_in:mp.Queue, queue_out:mp.Queue, nproc_in:int, nproc_out:int):
+        super().__init__()
+        self.queue_in = queue_in
+        self.queue_out = queue_out
+        self.nproc_in = nproc_in
+        self.nproc_out = nproc_out
+
+    def run(self):
+        for _ in range(self.nproc_in):
+            for item in iter(self.queue_in.get, None):
+                self.queue_out.put(item)
+        for _ in range(self.nproc_out):
+            self.queue_out.put(None)
+
+
+class Batcher(mp.Process):
+    def __init__(self, queue_in: mp.Queue, queue_out: mp.Queue, batch_size:int):
+        super().__init__()
+        self.queue_in = queue_in
+        self.queue_out = queue_out
+        self.batch_size = batch_size
+
+    def run(self):
+        batch = []
+        for item in iter(self.queue_in.get, None):
+            batch.append(item)
+            if len(batch) >= self.batch_size:
+                self.queue_out.put(batch)
+                batch = []
+        if batch:
+            self.queue_out.put(batch)
+        self.queue_out.put(None)
+
+
+class MultiWorkerPipeline:
+    def __init__(self, queue_in:mp.Queue, queue_out: mp.Queue, work, nproc:int = None):
+        self.queue_in = queue_in
+        self.qin = mp.Queue() # intermediate
+        self.queue_out = queue_out
+        self.qout = mp.Queue() # intermediate
+        self.nproc = nproc or mp.cpu_count()
+        self.work = work
+
+    def start(self):
+        Adapter(self.queue_in, self.qin, 1, self.nproc).start()
+        Adapter(self.qout, self.queue_out, self.nproc, 1).start()
+
+        for _ in range(self.nproc):
+            Pipeline(self.qin, self.qout, self.work).start()
+
+
+class FileReader(mp.Process):
+    def __init__(self, filename:str, queue:mp.Queue):
+        super().__init__()
+        self.filename = filename
+        self.queue = queue
+
+    def run(self):
+        with open(self.filename, 'r', encoding='utf-8') as f:
+            for line in f:
+                self.queue.put(line)
+            self.queue.put(None)
+
+
+def line_process(line):
+    tokens = line.split('\t')
+    vector = tuple(float(x) for x in tokens[1].split('|'))
+    return tokens[0], vector
+
+
+def batch_process(batch):
+    return [line_process(line) for line in batch]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", required=True)
+    parser.add_argument('--nproc', type=int, required=True)
+    parser.add_argument('--batch_size', type=int, required=True)
+    args = parser.parse_args()
+
+    q1 = mp.Queue()
+    FileReader(args.file, q1).start()
+    q2 = mp.Queue()
+    Batcher(q1, q2, args.batch_size).start()
+    q3 = mp.Queue()
+    MultiWorkerPipeline(q2, q3, batch_process, args.nproc).start()
+
+    for batch in iter(q3.get, None):
+        print(len(batch))
